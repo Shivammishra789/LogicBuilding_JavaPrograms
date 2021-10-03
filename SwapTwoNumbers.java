7562c0c580ce749deed8b7b69fc007c6cd362e49
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-03 23:07:23 +0530

    swapping two numbers

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,18 @@
+public class SwapTwoNumbers {
+	public static void main(String[] args) {
+	
+		
+	int x=31;
+	int y=24;
+
+	System.out.println("Numbers to be swapped are "+x+" "+y);
+	
+	int a=(x+y-x);
+			  //swapping of numbers 
+	int b=(x+y-y);
+	
+	System.out.println("the swapped numbers are " +a +" "+ +b);
+		
+	}
+}
+
